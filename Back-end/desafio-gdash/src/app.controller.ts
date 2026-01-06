cbd43a9901d5a0398daefd04c0ddbaab08e552c7
Josadack <josadaqueferreira@outlook.com>
2026-01-06 11:31:10 -0300

    Update de controler de versao

--- Back-end/desafio-gdash/src/app.controller.ts
+++ Back-end/desafio-gdash/src/app.controller.ts
@@ -1,15 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  @ApiExcludeEndpoint()
   @Get('/')
   health() {
     return {
       app: 'GDASH API',
       status: 'online',
-      version: '1.0.0',
       basePath: '/api',
       swagger: '/swagger',
       timestamp: new Date().toISOString(),
