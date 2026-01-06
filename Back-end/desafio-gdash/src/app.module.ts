7e62ed1695a0e462e1a4a1a940e2c83826a1bcf8
Josadack <josadaqueferreira@outlook.com>
2026-01-06 11:12:47 -0300

    Update

--- Back-end/desafio-gdash/src/app.module.ts
+++ Back-end/desafio-gdash/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-
 import { AppController } from "./app.controller";
 import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
